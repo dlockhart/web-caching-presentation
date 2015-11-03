0f9abec651a51089126378fc40358739c9293cda
Dave Lockhart <dave.lockhart@d2l.com>
2015-11-03 17:49:49 -0500

    adding labels and duration text

--- src/step.js
+++ src/step.js
@@ -1,26 +1,49 @@
 var React = require('react');
 
 var step = React.createClass({
-	componentDidMount: function() {
-		//var length = this.refs.line.getTotalLength();
-		//console.log(length);
-	},
 	render: function() {
+
+		var data = this.props.data;
+
 		var colWidth = Math.floor(100 / this.props.cols);
-		var dir = this.props.data.start < this.props.data.end ? 'R' : 'L';
-		var x1 = this.props.data.start * colWidth - (colWidth / 2);
-		var x2 = this.props.data.end * colWidth - (colWidth / 2);
-		if (dir === 'R' && this.props.data.end !== this.props.cols) {
+		var dir = data.start < data.end ? 'R' : 'L';
+		var x1 = data.start * colWidth - (colWidth / 2);
+		var x2 = data.end * colWidth - (colWidth / 2);
+
+		if (dir === 'R' && data.end !== this.props.cols) {
 			x1--;
-		} else if (dir === 'L' && this.props.data.end !== 1) {
+		} else if (dir === 'L' && data.end !== 1) {
 			x2++;
 		}
-		var y = this.props.data.row * 100 - 50;
+		var y = data.row * 100 - 50;
+
 		var className = 'lineArrow' + dir;
 		var style = {
 			display: this.props.isVisible ? 'inline' : 'none'
 		};
-		return <line ref="line" className={className} x1={x1 + '%'} y1={y} x2={x2 + '%'} y2={y} style={style} />;
+
+		var label = null;
+		if (data.label) {
+			var labelX = (x2 - x1) / 2 + x1;
+			label = <text x={labelX + '%'} y={y - 8} textAnchor="middle">
+				{data.label}
+			</text>;
+		}
+
+		var duration = null;
+		if (data.duration) {
+			var durationX = (x2 - x1) / 2 + x1;
+			duration = <text x={durationX + '%'} y={y + 17} textAnchor="middle">
+				{data.duration + 'ms'}
+			</text>;
+		}
+
+		return <g style={style}>
+				{label}
+				<line ref="line" className={className} x1={x1 + '%'} y1={y} x2={x2 + '%'} y2={y} />
+				{duration}
+			</g>;
+
 	}
 });
 
