e0015a838235d84632d365c599d548982284a1d5
Dave Lockhart <dave.lockhart@d2l.com>
2015-11-05 21:19:22 -0500

    column icons

--- src/column.js
+++ src/column.js
@@ -2,11 +2,26 @@ var React = require('react');
 
 var column = React.createClass({
 	render: function() {
-		var lineX = (this.props.index + 1) * this.props.width + '%';
-		var nameX = Math.floor(this.props.index * this.props.width + this.props.width / 2) + '%';
-		var name = this.props.hidden ? null : <text x={nameX} y="20" textAnchor="middle">{this.props.name}</text>;
-		var line = this.props.isLast ? null : <line className="col-sep" x1={lineX} y1="0" x2={lineX} y2="100%" />;
-		return <g>{line}{name}</g>;
+
+		var centreX = Math.floor(this.props.index * this.props.width + this.props.width / 2) + '%';
+
+		var name = null;
+		var image = null;
+		if (!this.props.hidden) {
+			name = <text x={centreX} y="100" textAnchor="middle">{this.props.name}</text>;
+			if (this.props.image) {
+				image = <g dangerouslySetInnerHTML={{__html: '<image x=' + centreX + ' y=0 width="75" height="75" xlink:href="images/' + this.props.image + '" transform="translate(-37,0)"/>'}} />;
+			}
+		}
+
+		var line = null;
+		if (!this.props.isLast) {
+			var lineX = (this.props.index + 1) * this.props.width + '%';
+			line = <line x1={lineX} y1="0" x2={lineX} y2="100%" />;
+		}
+
+		return <g className="column">{line}{image}{name}</g>;
+
 	}
 });
 
